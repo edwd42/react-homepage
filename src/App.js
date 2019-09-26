d6f53e163f83ebd2c5a868bc1d5875994e90af09
edwd42 <edwd42@gmail.com>
2019-09-26 15:24:59 -0400

    add components/Layout.js to wrap around components in App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
 import React, { Component } from "react";
 import { HashRouter, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Home } from "./components/Home";
+import { Layout } from "./components/Layout";
 
 class App extends Component {
   render() {
     return (
-      <HashRouter basename="/">
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Me</Link>
-            </li>
-          </ul>
-          <hr />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            {/* <Route path="*" component={() => "404 NOT FOUND"} /> */}
-          </Switch>
-        </div>
-      </HashRouter>
+      <>
+        <Layout>
+          <HashRouter basename="/">
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About Me</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact Me</Link>
+                </li>
+              </ul>
+              <hr />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+              </Switch>
+            </div>
+          </HashRouter>
+        </Layout>
+      </>
     );
   }
 }
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
 
 export default App;
