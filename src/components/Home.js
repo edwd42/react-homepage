15f4f7f1e66c1e1e0ee090185536611d66faf0b9
edwd42 <edwd42@gmail.com>
2019-09-27 17:11:14 -0400

    add banner to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,52 @@ import "../assets/css/home.css";
 import bgimage from "../assets/images/BG_1.jpg";
 
 export const Home = () => (
-  <div>
+  <>
     <img src={bgimage} alt="bgimage" id="bg" />
-  </div>
+    <div className="banner">
+      <div className="banner-text">
+        <h1>Edward Dupre</h1>
+        <h1>Full Stack Web Developer</h1>
+        <hr />
+        <p>
+          HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS |
+          Express | MongoDB
+        </p>
+
+        <div className="social-links">
+          {/* LinkedIn */}
+          <a
+            href="http://linkedin.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
+
+          {/* Github */}
+          <a href="http://github.com" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+
+          {/* Freecodecamp */}
+          <a
+            href="http://freecodecamp.org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fa fa-free-code-camp" aria-hidden="true" />
+          </a>
+
+          {/* Twitter */}
+          <a
+            href="http://twitter.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fa fa-twitter-square" aria-hidden="true" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
 );
