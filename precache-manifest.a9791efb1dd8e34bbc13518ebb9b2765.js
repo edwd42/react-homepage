1370d963bb3b52e8a6e11f22cf6c9473ec3d147b
edwd42 <edwd42@gmail.com>
2019-09-28 21:06:45 -0400

    Updates

---
+++ precache-manifest.a9791efb1dd8e34bbc13518ebb9b2765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c64f83f7da763ff4875e04efffa5d3",
+    "url": "/react-homepage/index.html"
+  },
+  {
+    "revision": "7e40dbf6a5025a2c366a",
+    "url": "/react-homepage/static/css/main.68a5b617.chunk.css"
+  },
+  {
+    "revision": "819897f41816e74d618c",
+    "url": "/react-homepage/static/js/2.851c96dc.chunk.js"
+  },
+  {
+    "revision": "7e40dbf6a5025a2c366a",
+    "url": "/react-homepage/static/js/main.781020f9.chunk.js"
+  },
+  {
+    "revision": "6271fff6ab2bc24b7117",
+    "url": "/react-homepage/static/js/runtime-main.712f043d.js"
+  },
+  {
+    "revision": "8c3f5e8d1d5c17870dc3875ce9583437",
+    "url": "/react-homepage/static/media/BG_1.8c3f5e8d.jpg"
+  }
+]);
