fc20d7ad8b4e092856b2f4e299a6105c4b20d58a
edwd42 <edwd42@gmail.com>
2019-09-28 20:52:40 -0400

    Updates

---
+++ precache-manifest.c5c737af844d65d46681789ed99f1ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a92e1b8fa7130702aa154cefc4a9a8b",
+    "url": "/react-homepage/index.html"
+  },
+  {
+    "revision": "8a0a04bc93697da7b9cd",
+    "url": "/react-homepage/static/css/main.68a5b617.chunk.css"
+  },
+  {
+    "revision": "0fd023a136be9bf0c562",
+    "url": "/react-homepage/static/js/2.f62fadf2.chunk.js"
+  },
+  {
+    "revision": "8a0a04bc93697da7b9cd",
+    "url": "/react-homepage/static/js/main.b53c16df.chunk.js"
+  },
+  {
+    "revision": "495a2e50f1a5dd74e5ce",
+    "url": "/react-homepage/static/js/runtime-main.1b82b006.js"
+  },
+  {
+    "revision": "8c3f5e8d1d5c17870dc3875ce9583437",
+    "url": "/react-homepage/static/media/BG_1.8c3f5e8d.jpg"
+  }
+]);
--- src/components/About.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-
-export const About = () => (
-  <Container>
-    <h2>About</h2>
-  </Container>
-);
