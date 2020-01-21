93dbf606affeb8aa527b33f9cb33bcef18009c00
edwd42 <edwd42@gmail.com>
2020-01-21 18:18:23 -0500

    add CareerDevs certificates of completion to CareerDevs.js page

--- src/components/CareerDevs.js
+++ src/components/CareerDevs.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "../assets/css/careerdevs.css";
 import CareerDevsClassroom from "../assets/images/CareerDevsClassroom.png";
+import CareerDevsCohortOnRoofTop from "../assets/images/CareerDevsCohortOnRoofTop.jpg";
+import CDAdvancedCertificate from "../assets/images/CD_certificates/CDAdvancedCertificate.png";
+import CDIntermediateCertfiicate from "../assets/images/CD_certificates/CDIntermediateCertfiicate.jpg";
+import CDIntroductionCertfiicate from "../assets/images/CD_certificates/CDIntroductionCertfiicate.jpg";
 
 export const CareerDevs = () => (
   <>
     <main>
-      <div id="home" className="pad-section home"></div>
+      <div id="home" className="home pad-section">
+        <div className="certificates">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="certs">
+                <img src={CDIntroductionCertfiicate} alt="" />
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="certs">
+                <img src={CDIntermediateCertfiicate} alt="" />
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="certs">
+                <img src={CDAdvancedCertificate} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <div id="about" className="pad-section">
+      <div id="about" className="about pad-section">
         <div className="container">
           <div className="row">
             <div className="col-sm-6">
@@ -29,6 +53,12 @@ export const CareerDevs = () => (
         </div>
       </div>
 
+      <div className="pad-section">
+        <div className="container">
+          <img src={CareerDevsCohortOnRoofTop} alt="" />
+        </div>
+      </div>
+
       <div id="services" className="pad-section">
         <div className="container">
           <h2 className="text-center">My Projects</h2> <hr />
@@ -37,6 +67,7 @@ export const CareerDevs = () => (
               <a
                 href="https://edwd42.github.io/NewportDataProject/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fontawesomeicon fas fa-cloud"> </i>
                 <h4>Leaflet App</h4>
@@ -46,19 +77,28 @@ export const CareerDevs = () => (
               <a
                 href="https://code-42.github.io/random_quote_machine/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fontawesomeicon fas fa-atom"> </i>
                 <h4>Random Quotes</h4>
               </a>
             </div>
             <div className="col-sm-3 col-xs-6">
-              <a href="https://code-42.github.io/product_page/" target="_blank">
+              <a
+                href="https://code-42.github.io/product_page/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fontawesomeicon fas fa-blender-phone"> </i>
                 <h4>Product Page</h4>
               </a>
             </div>
             <div className="col-sm-3 col-xs-6">
-              <a href="http://18.191.196.165/" target="_blank">
+              <a
+                href="http://18.191.196.165/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fontawesomeicon fab fa-envira"> </i>
                 <h4>Code Book</h4>
               </a>
@@ -67,49 +107,39 @@ export const CareerDevs = () => (
         </div>
       </div>
 
-      <div id="information" className="pad-section">
+      {/* <div id="information" className="about pad-section">
         <div className="container">
           <div className="row">
-            <div className="col-sm-6">
+            <div className="col-sm-4">
               <div className="panel panel-default">
                 <div className="panel-heading">
-                  <h2 className="panel-title">
-                    Teaching people powerful things.
-                  </h2>
+                  <h2 className="panel-title"></h2>
                 </div>
-                <div className="panel-body lead">
-                  CareerDevs teaches its students a wide array of in-demand
-                  skills including web development, software development,
-                  algorithmic thinking and problem solving and soft skills. We
-                  empower students to empower themselves and each other to
-                  become skilled workers and thinkers who are adept across a
-                  wide variety of technologies, working styles and problems.
-                  CareerDevs enables workers to enter new jobs with the ability
-                  to not just succeed, but thrive.
-                </div>
+                <div className="panel-body lead certs"></div>
               </div>
             </div>
-            <div className="col-sm-6">
+
+            <div className="col-sm-4">
               <div className="panel panel-default">
                 <div className="panel-heading">
-                  <h2 className="panel-title">
-                    Learn in a supported, collaborative environment.
-                  </h2>
+                  <h2 className="panel-title"></h2>
                 </div>
                 <div className="panel-body lead"></div>
-                CareerDevs classrooms start out teacher-led but become driven by
-                students’ talents and interests as cohorts advance through the
-                program. Students rely on each other to solve problems,
-                inculcating a culture of self-learning and empowerment backed up
-                by teachers who don’t tell you the answer to problems, but the
-                answer about how to solve them.
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h2 className="panel-title"></h2>
+                </div>
+                <div className="panel-body lead"></div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </div> */}
 
-    <style></style>
+
+    </main>
   </>
 );
